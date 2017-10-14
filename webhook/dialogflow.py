9e662a859d3859c4fe16997797bf15d1249a9057
eastfar1324 <31936982+eastfar1324@users.noreply.github.com>
2017-10-15 01:45:08 +0900

    17.10.15 01:45 before add logging
    17.10.15 01:45 before add logging

--- webhook/dialogflow.py
+++ webhook/dialogflow.py
@@ -1,6 +1,7 @@
 import os.path
 import sys
 import json
+import logging
 try:
     import apiai
 except ImportError:
@@ -14,9 +15,11 @@ class DialogFlow:
     def __init__(self):
         self.dialog = apiai.ApiAI('9e2f805b26a44df3955374bb8278e848')
 
-    def get_speech_request(self, request):
-        json_request = json.loads((request.body).decode('utf-8'))
-        return json_request['content']
+    def get_json(self, http_request):
+        return json.loads((http_request.body).decode('utf-8'))
+
+    def get_speech_request(self, request_json):
+        return request_json['content']
 
     def get_speech_response(self, speech_request):
         request = self.dialog.text_request()
@@ -25,4 +28,17 @@ class DialogFlow:
 
         response = request.getresponse()
         json_response = json.loads(response.read().decode('utf-8'))
+
         return json_response["result"]["fulfillment"]["speech"]
+
+    def get_webhook_response(self, speech_request):
+        logging.debug(speech_request)
+        speech_response = self.get_speech_response(speech_request)
+        logging.debug(speech_response)
+        return {
+            "speech": speech_response,
+            "displayText": speech_response,
+            "data": {},
+            # "contextOut": [],
+            "source": "ssu-notice"
+        }
--- webhook/main.py
+++ webhook/main.py
@@ -1,5 +1,24 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from django.http import JsonResponse
 from dialogflow import DialogFlow
+import json
 
+
+@csrf_exempt
 def index(request):
-    return HttpResponse(DialogFlow().get_speech_response('show me notices from yesterday'))
+    dialog_flow = DialogFlow()
+    if request.POST:
+        json_request = dialog_flow.get_json(request)
+        speech_request = dialog_flow.get_speech_request(json_request)
+        webhook_response = dialog_flow.get_webhook_response(speech_request)
+
+        return HttpResponse(
+            JsonResponse(json.dump(webhook_response)),
+            content_type="application/json; charset=utf-8",
+        )
+    else:
+        return HttpResponse(
+            DialogFlow().get_speech_response('show me notices from yesterday'),
+            content_type="text/html; charset=utf-8",
+        )

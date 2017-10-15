ac7bc5d6b6b11bf271365bac22b2689d2eeece63
eastfar1324 <31936982+eastfar1324@users.noreply.github.com>
2017-10-16 03:46:59 +0900

    17.10.16 03:45 beautifulsoup added
    17.10.16 03:45 beautifulsoup added

--- ssu_notice/urls.py
+++ ssu_notice/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from webhook.main import index
+from webcrawling.test import test
 
 urlpatterns = [
     url(r'^$', index),
+    url(r'^crawling', test),
     url(r'^admin/', include(admin.site.urls)),
 ]
